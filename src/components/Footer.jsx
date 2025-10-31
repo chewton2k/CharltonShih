898ec0eb05bed5dc47af2d3a746d989ebbc0e606
Charlton Shih <168577923+chewton2k@users.noreply.github.com>
2025-10-31 01:30:19 -0700

    fixed footer issues and added hugging face account

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => (
     </div>
     <div className="footer-right">
       <a href="Charlton_Shih_Resume.pdf" target="_blank">Resume</a>
+      <a href="https://huggingface.co/chewton2k" target="_blank" rel="nonoopener noreferrer">Hugging Face</a>
       <a href="https://www.linkedin.com/in/charlton-shih/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
       <a href="https://github.com/chewton2k" target="_blank" rel="noopener noreferrer">GitHub</a>
       <a href="mailto:charltonshih645@g.ucla.edu">Email</a>
